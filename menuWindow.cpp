57eb6fd5468720a94d24c83eda9efffa450cfedb
Sorax37 <52798869+Sorax37@users.noreply.github.com>
2020-12-07 00:05:12 +0300

    Add files via upload
    Клиент тетрис, без реализации сервера. 
    Сервер в разработке

---
+++ menuWindow.cpp
@@ -0,0 +1,12 @@
+#include "menuWindow.h"
+
+#include <Windows.h>
+
+using namespace CLRTetris;
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew menuWindow);
+    return 0;
+}
